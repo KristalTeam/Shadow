6f5ece4da3ca2dc53f3d52f7c592d16f07180d95
NyakoFox <nyakowofox@gmail.com>
2026-07-21 02:16:38 -0300

    set deploymentId

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@ import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx']
+    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    deploymentId: process.env.DEPLOYMENT_VERSION
 }
 
 const withMDX = createMDX({
